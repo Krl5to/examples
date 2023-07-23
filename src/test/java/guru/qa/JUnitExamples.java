e6c84195bcedd38fcfd855447e967ad1f4d80296
Krl5to <i.petretskiy@gmail.com>
2023-07-23 21:37:41 +0300

    Done Examples

--- src/test/java/guru/qa/JUnitExamples.java
+++ src/test/java/guru/qa/JUnitExamples.java
@@ -1,12 +1,46 @@
 package guru.qa;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
 
 public class JUnitExamples {
 
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("beforeAll()");
+    }
+
+    @AfterAll
+    static void AfterAll() {
+        System.out.println("beforeAll()");
+    }
+
+    @BeforeEach
+    void BeforeEach() {
+        Configuration.browser = "firefox";
+        System.out.println("    BeforeEach()");
+    }
+
+    @AfterEach
+    void AfterEach() {
+        System.out.println("    AfterEach()");
+    }
+
     @Test
     void firstTest() {
+        System.out.println("        firstTest()");
+        Assertions.assertTrue(3 > 2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("        secondTest()");
+        Assertions.assertTrue(3 > 2);
+    }
+
+    @Test
+    void thirdTest() {
+        System.out.println("        thirdTest()");
         Assertions.assertTrue(3 > 2);
     }
 
